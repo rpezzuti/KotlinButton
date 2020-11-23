d0428344283ba2f013042fc52d26fa2e22ad9403
Rhett Pezzuti <rpezzuti@gmail.com>
2020-11-22 23:46:45 -0800

    all navigation

--- app/src/main/java/rhett/pezzuti/kotlinbutton/game/GameViewModel.kt
+++ app/src/main/java/rhett/pezzuti/kotlinbutton/game/GameViewModel.kt
@@ -1,10 +1,25 @@
 package rhett.pezzuti.kotlinbutton.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private val _eventPressButton = MutableLiveData<Boolean>()
+    val eventPressButton : LiveData<Boolean>
+        get() =  _eventPressButton
 
+    init {
+        _eventPressButton.value = false
+    }
+
+    fun onPressButton(){
+        _eventPressButton.value = true
+    }
+    fun onDonePressButton(){
+        _eventPressButton.value = false
+    }
 
 
 
--- app/src/main/java/rhett/pezzuti/kotlinbutton/sound/SoundViewModel.kt
+++ app/src/main/java/rhett/pezzuti/kotlinbutton/sound/SoundViewModel.kt
@@ -1,9 +1,24 @@
 package rhett.pezzuti.kotlinbutton.sound
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SoundViewModel : ViewModel(){
 
+    private val _eventGoHome = MutableLiveData<Boolean>()
+    val eventGoHome : LiveData<Boolean>
+        get() = _eventGoHome
 
+    init {
+        _eventGoHome.value = false
+    }
+
+    fun goHome(){
+        _eventGoHome.value = true
+    }
+    fun doneGoingHome(){
+        _eventGoHome.value = false
+    }
 
 }
