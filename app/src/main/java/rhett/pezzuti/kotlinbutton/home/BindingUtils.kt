bd3d1894dc1c374d917ae1ea62e502b37416ee11
Rhett Pezzuti <rpezzuti@gmail.com>
2020-12-01 21:31:05 -0800

    adding data binding to viewholder using a binding adapter

---
+++ app/src/main/java/rhett/pezzuti/kotlinbutton/home/BindingUtils.kt
@@ -0,0 +1,44 @@
+package rhett.pezzuti.kotlinbutton.home
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import rhett.pezzuti.kotlinbutton.R
+import rhett.pezzuti.kotlinbutton.database.ButtonPreset
+
+@BindingAdapter("presetMessage")
+fun TextView.setPresetMessage(item: ButtonPreset?){
+    item?.let {
+        text = item.text
+    }
+}
+
+@BindingAdapter("presetImage")
+fun ImageView.setPresetImage(item: ButtonPreset){
+    setImageResource(when (item.picture){
+        1 -> R.drawable.heart
+        2 -> R.drawable.awesome
+        3 -> R.drawable.i_miss_you
+        4 -> R.drawable.im_a_ball
+        5 -> R.drawable.sorry
+        6 -> R.drawable.sprout
+        7 -> R.drawable.stop
+        8 -> R.drawable.tongue
+        9 -> R.drawable.troll
+        10 -> R.drawable.yes
+        else -> R.drawable.ic_launcher_background
+    })
+}
+
+@BindingAdapter("presetSound")
+fun TextView.setPresetSound(item: ButtonPreset?){
+    item?.let {
+        text = (when (item.sound){
+            1 -> "Meep Merp"
+            2 -> "Chimes"
+            3 -> "Fart"
+            4 -> "TeeHee"
+            else -> "Sound File Not Chosen"
+        })
+    }
+}
--- app/src/main/java/rhett/pezzuti/kotlinbutton/home/PresetAdapter.kt
+++ app/src/main/java/rhett/pezzuti/kotlinbutton/home/PresetAdapter.kt
@@ -1,28 +1,27 @@
 package rhett.pezzuti.kotlinbutton.home
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-import rhett.pezzuti.kotlinbutton.R
+
 import rhett.pezzuti.kotlinbutton.database.ButtonPreset
+import rhett.pezzuti.kotlinbutton.databinding.ButtonPresetViewBinding
 
-class PresetAdapter : RecyclerView.Adapter<PresetAdapter.ViewHolder>() {
+class PresetAdapter : ListAdapter<ButtonPreset,PresetAdapter.ViewHolder>(SleepNightDiffCallBack()) {
 
-    /** this is the data to be displayed, and the setter below is used to tell the RV when data has changed. **/
+   /* *//** this is the data to be displayed, and the setter below is used to tell the RV when data has changed. **//*
     var data = listOf<ButtonPreset>()
         set(value) {
             field = value
             notifyDataSetChanged()
-    }
+    }*/
 
-    override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
     }
 
@@ -30,42 +29,34 @@ class PresetAdapter : RecyclerView.Adapter<PresetAdapter.ViewHolder>() {
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val res = itemView.context.resources
-
-        private val image: ImageView = itemView.findViewById(R.id.button_preset_image)
-        private val message: TextView = itemView.findViewById(R.id.button_preset_text)
-        private val sound: TextView = itemView.findViewById(R.id.button_preset_sound)
+    class ViewHolder private constructor(val binding: ButtonPresetViewBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bind(item: ButtonPreset){
-            image.setImageResource(R.drawable.ic_launcher_background)
-
-            when (item.picture){
-                1 -> image.setImageResource(R.drawable.heart)
-                2 -> image.setImageResource(R.drawable.awesome)
-                3 -> image.setImageResource(R.drawable.i_miss_you)
-                4 -> image.setImageResource(R.drawable.im_a_ball)
-                5 -> image.setImageResource(R.drawable.sorry)
-                6 -> image.setImageResource(R.drawable.sprout)
-                7 -> image.setImageResource(R.drawable.stop)
-                8 -> image.setImageResource(R.drawable.tongue)
-                9 -> image.setImageResource(R.drawable.troll)
-                10 -> image.setImageResource(R.drawable.yes)
-            }
-            message.text = item.text
-            sound.text = item.sound.toString()
+            binding.preset = item
+            binding.executePendingBindings()
         }
 
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 /** we're inflating a new view and returning it inside of a holder **/
-                val view = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.button_preset_view, parent, false)
-                return ViewHolder(view)
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ButtonPresetViewBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
-
-
     }
+
+
+    class SleepNightDiffCallBack : DiffUtil.ItemCallback<ButtonPreset>() {
+
+        override fun areItemsTheSame(oldItem: ButtonPreset, newItem: ButtonPreset): Boolean {
+            return oldItem.presetId == newItem.presetId
+        }
+
+        override fun areContentsTheSame(oldItem: ButtonPreset, newItem: ButtonPreset): Boolean {
+            return oldItem == newItem
+        }
+    }
+
 }
